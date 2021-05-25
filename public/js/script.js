2ac80ff57b466e603d4cae747406e186216f4da3
vijayatm <atmvijay1993@gmail.com>
2021-05-25 22:52:17 +0530

    language, error message, methods

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-const baseApi = 'https://api.github.com/users/'
+const baseApi = 'https://api.github.com/'
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
@@ -8,12 +8,11 @@ const followersCard = document.getElementById('followers');
 const repoCard = document.getElementById('repositories');
 
 
-getUser('naveenanimation20');
-
+// getUser('naveenanimation20');
 
 
 async function getUser(username){
-    const resp = await fetch(baseApi +username);
+    const resp = await fetch(baseApi +'users/'+username);
     const response = await resp.json();
 
     
@@ -24,7 +23,8 @@ async function getUser(username){
    getFollowersAddToCard_Followers(username);
 
    createRepositoriesCard();
-   getReposAddToCard_Repositories(username);    
+   getReposAddToCard_Repositories(username);   
+   
 }
 
 
@@ -87,28 +87,37 @@ function createFollowersCard(){
 }
 
 async function getFollowersAddToCard_Followers(username){
-    const respF = await fetch(baseApi +username+"/followers");
+    const respF = await fetch(baseApi +'users/'+username+"/followers");
     const response = await respF.json();
 
     
     const followersElement = document.getElementById('followers-avatars');  
 
-    response
-    .forEach((follower)=>{
-        const aFollElement = document.createElement('a');
-        aFollElement.href=follower.html_url;
-        aFollElement.classList.add('users-list');
-        aFollElement.target="_blank";
+    if(response.length ==0){
+        console.log("length is 0");
+        const aFollElement = document.createElement('h2');
+        aFollElement.classList.add('err-msg');
+        aFollElement.innerHTML = "You don't have any followers.<br> Inspire people";
+        followersElement.appendChild(aFollElement);
 
-        const follElement = document.createElement('img');
-        follElement.classList.add('fol-img');
-        follElement.src = follower.avatar_url;
-        follElement.title = follower.login;
+    }else{
+        response
+        .forEach((follower)=>{
+            const aFollElement = document.createElement('a');
+            aFollElement.href=follower.html_url;
+            aFollElement.classList.add('users-list');
+            aFollElement.target="_blank";
 
-        aFollElement.appendChild(follElement); 
-        followersElement.appendChild(aFollElement); 
+            const follElement = document.createElement('img');
+            follElement.classList.add('fol-img');
+            follElement.src = follower.avatar_url;
+            follElement.title = follower.login;
 
-    });
+            aFollElement.appendChild(follElement); 
+            followersElement.appendChild(aFollElement); 
+
+        });
+    }
 }
 
 
@@ -119,9 +128,7 @@ function createRepositoriesCard(repoRes){
         <div class="fol-title-block">
             <h1> Repositories List </h1>
             <div id="repo-list"></div>
-        </div>    
-            
-        
+        </div>   
     </div>`
 
     repoCard.innerHTML = repositoriesCardHTML;
@@ -129,7 +136,7 @@ function createRepositoriesCard(repoRes){
 
 
 async function getReposAddToCard_Repositories(username){
-    const respRaw = await fetch(baseApi +username+"/repos");
+    const respRaw = await fetch(baseApi +'users/'+username+"/repos");
     const response = await respRaw.json();
 
 
@@ -141,22 +148,41 @@ async function getReposAddToCard_Repositories(username){
         const repElement = document.createElement('div');
         repElement.classList.add('repos');
 
+        repElement.append(addNameTimeLangToCard(repo));
+        repElement.append(addWatchStarForkToCard(repo));
+       
+        RepositoriesElement.appendChild(repElement); 
+    });
+   
+}
+
+function addNameTimeLangToCard(repo){
+    const nameTimeLangHTML = document.createElement('div');
         
-        const nameLangHTML = document.createElement('div');
+    const repoNameHTML = document.createElement('a');
+    repoNameHTML.innerHTML = repo.name;
+    repoNameHTML.href=repo.html_url;
+    nameTimeLangHTML.appendChild(repoNameHTML);
 
-        const repoNameHTML = document.createElement('a');
-        repoNameHTML.innerHTML = repo.name;
-        repoNameHTML.href=repo.html_url;
-        nameLangHTML.appendChild(repoNameHTML);
-        repElement.appendChild(nameLangHTML);
+    const timeCreateHTML = document.createElement('span');
+    timeCreateHTML.classList.add('repoCreTime');
+    timeCreateHTML.innerHTML = repo.created_at;
+    nameTimeLangHTML.appendChild(timeCreateHTML);
 
+    const langCont = document.createElement('h3');
+    const langHTML = document.createElement('span');
+    langHTML.classList.add("top-language");
+    if(repo.language!='null'){
+    langHTML.innerHTML = repo.language;
+    }
+    langCont.append(langHTML);
+    nameTimeLangHTML.appendChild(langCont);
 
-        const langCreateHTML = document.createElement('span');
-        langCreateHTML.classList.add('reposLang');
-        langCreateHTML.innerHTML = repo.created_at;
-        nameLangHTML.appendChild(langCreateHTML);
+    return nameTimeLangHTML;
+}
 
-        const threeLists = document.createElement('div');
+function addWatchStarForkToCard(repo){
+    const threeLists = document.createElement('div');
         const ulItems = document.createElement('ul');
         ulItems.classList.add('wa-st-fo');
         var liItems_1 =document.createElement('li');
@@ -176,40 +202,10 @@ async function getReposAddToCard_Repositories(username){
         ulItems.append(liItems_1);
         ulItems.append(liItems_2);
         ulItems.append(liItems_3);
-        threeLists.append(ulItems)
-        repElement.append(threeLists);
-       
-       
+        threeLists.append(ulItems);
 
-
-        RepositoriesElement.appendChild(repElement); //appending all to main repo-list class div
-       
-        // const repListUL = document.createElement('ul');
-        // repListUL.id = 'reposListID';
-        // var li = document.createElement('li');
-        
-        // const aFollElement = document.createElement('a');
-        // aFollElement.href=follower.html_url;
-        // aFollElement.classList.add('users-list');
-        // aFollElement.target="_blank";
-
-        // const follElement = document.createElement('img');
-        // follElement.classList.add('fol-img');
-        // follElement.src = follower.avatar_url;
-        
-        // follElement.title = follower.login;
-       
-          //appending the lang and time with respos class div
-        
-
-    });
+        return threeLists;
 }
-
-// function convertedTime(date){
-//     // var date = "2019-07-14T18:30:00.000Z";
-// var dateTime = ZonedDateTime.parse(date);
-// return dateTime.withZoneSameInstant(ZoneId.of("Asia/Calcutta")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss a"));
-// }
 
 
 form.addEventListener('submit', e=>{
